606ee622379c12bb426cebc9a290ea4db9003f61
Antonio Raimundo <antoniorrm@Mac-mini-Dj.local>
2020-01-17 16:33:40 -0300

    Projeto iniciado faltando logica e pouco de style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './global.css';
+import './Sidebar.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong className="" >Cadastrar</strong>
+        <form>
+          <div className="input-block" >
+            <label htmlFor="github_username"> Usuário do Github</label>
+            <input name="github_username" id="github_username" required></input>
+          </div>
+          <div className="input-block" >
+            <label htmlFor="techs">Tecnologias  </label>
+            <input name="techs" id="techs" required></input>
+          </div>
+
+          <div className="input-group">
+            <div className="input-block" >
+              <label htmlFor="longitude"> Longitude</label>
+              <input name="longitude" id="longitude" required></input>
+            </div>
+
+            <div className="input-block" >
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required></input>
+            </div>
+          </div>
+          <button type="submit" >Salvar</button>
+        </form>
+      </aside>
+
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/6803917?s=460&v=4" alt="" />
+              <div className="user-info">
+                <strong>Antonio Raimundo</strong>
+                <span>ReactJs, NodeJs, Adonisjs</span>
+              </div>
+            </header>
+            <p>💻 🎧 ⚫️ Graduated in Software Engineering at Federal University of Ceará</p>
+            <a href="https://github.com/antoniorrm">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/6803917?s=460&v=4" alt="" />
+              <div className="user-info">
+                <strong>Antonio Raimundo</strong>
+                <span>ReactJs, NodeJs, Adonisjs</span>
+              </div>
+            </header>
+            <p>💻 🎧 ⚫️ Graduated in Software Engineering at Federal University of Ceará</p>
+            <a href="https://github.com/antoniorrm">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/6803917?s=460&v=4" alt="" />
+              <div className="user-info">
+                <strong>Antonio Raimundo</strong>
+                <span>ReactJs, NodeJs, Adonisjs</span>
+              </div>
+            </header>
+            <p>💻 🎧 ⚫️ Graduated in Software Engineering at Federal University of Ceará</p>
+            <a href="https://github.com/antoniorrm">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/6803917?s=460&v=4" alt="" />
+              <div className="user-info">
+                <strong>Antonio Raimundo</strong>
+                <span>ReactJs, NodeJs, Adonisjs</span>
+              </div>
+            </header>
+            <p>💻 🎧 ⚫️ Graduated in Software Engineering at Federal University of Ceará</p>
+            <a href="https://github.com/antoniorrm">Acessar perfil no Github</a>
+          </li>
+          
+        </ul>
+      </main>
     </div>
   );
 }
